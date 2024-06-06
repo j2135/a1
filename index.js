cf962b2767e9958ac77d7b1e3ce9af63f820a00d
j2135 <159898285+j2135@users.noreply.github.com>
2024-06-06 06:41:00 +0000

    REST API

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// GETリクエスト
 app.get('/', (req, res) => {
-  res.send('Hello World!松江高専万歳！')
-})
+  res.send({
+    msg:'GET request'
+  });
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// POSTリクエスト
+app.post('/', (req, res) => {
+  res.send({
+    msg:'POST request'
+  });
+});
+
+// PUTリクエスト
+app.put('/:id', (req, res) => {
+  res.send({
+    id: req.params.id,
+    msg:'PUT request'
+  });
+});
+
